64a110f5abd09c00997e0aea6ae274f58108ce78
tomy sincodigo <181906310+tomySinCodigo@users.noreply.github.com>
2025-04-13 17:11:04 -0400

    add Button to window

--- ventana.py
+++ ventana.py
@@ -4,6 +4,10 @@ import tkinter as tk
 ventana = tk.Tk()
 ventana.title("Ventana basica")
 
+numvar = 1
+btn = tk.Button(ventana, text=str(numvar), font=('Consolas 60'))
+btn.pack(expand=True, fill=tk.BOTH)
+
 
 ventana.geometry("200x300")
 ventana.mainloop()
