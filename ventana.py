83bd8b0877747aad6a30e127384e88ed8c3a655f
tomy sincodigo <181906310+tomySinCodigo@users.noreply.github.com>
2025-04-13 18:36:41 -0400

    add increment function to button

--- ventana.py
+++ ventana.py
@@ -4,10 +4,15 @@ import tkinter as tk
 ventana = tk.Tk()
 ventana.title("Ventana basica")
 
+def incrementNum():
+    global numvar
+    numvar += 1
+    btn.config(text=str(numvar))
+
 numvar = 1
-btn = tk.Button(ventana, text=str(numvar), font=('Consolas 60'))
+btn = tk.Button(ventana, text=str(numvar), font=('Consolas 60'), command=incrementNum)
 btn.pack(expand=True, fill=tk.BOTH)
 
 
-ventana.geometry("200x300")
+ventana.geometry("300x150")
 ventana.mainloop()
